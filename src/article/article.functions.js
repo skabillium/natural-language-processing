28f3a03355fb1a52587aa7a1e066a782f53f079b
skabi <skabillium@gmail.com>
2020-11-29 20:44:04 +0200

    Finish article functions

--- src/article/article.functions.js
+++ src/article/article.functions.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 const { isEmpty } = require('lodash');
-const natural = require('natural');
+const { JSDOM } = require('jsdom');
+const mongoose = require('mongoose');
 
+const { nlpService } = require('../nlp');
 const config = require('../config');
-// const Article = require('./article.model');
+const Article = require('./article.model');
 
 /**
  * Get New York Times home page and extract the article urls
@@ -14,30 +15,52 @@ async function get_nyt_urls() {
 		const response = await axios.get(config.urls.nyt);
 
 		// If no response errors occurred continue
-		if (response.status == 200) {
-			const $ = cheerio.load(response.data);
-
-			let urls = [];
-			const links = $('.assetWrapper');
-
-			// Extract article urls
-			for (let index = 0; index < links.length; index++) {
-				const link = links[index];
-				const anchor = $(link).find('a').attr('href');
-
-				if (!isEmpty(anchor) && config.regex.html.test(anchor)) {
-					if (anchor.includes('http')) {
-						urls.push(anchor);
-					} else {
-						urls.push(config.urls.nyt + anchor);
-					}
-				}
-			}
-
-			return urls;
-		} else {
+		if (response.status !== 200)
 			throw new Error('Something went wrong when fetching home page');
-		}
+
+		const dom = new JSDOM(response.data);
+
+		let urls = [];
+
+		// Select all article elements
+		dom.window.document.querySelectorAll('article').forEach((article) => {
+			// Select all anchor elements
+			article.querySelectorAll('a').forEach((anchor) => {
+				// Extract url from anchor
+				const link = anchor.getAttribute('href');
+				if (!isEmpty(link) && link.includes('.html')) {
+					// Check if link is relative or absolute and save accordingly
+					if (link.includes('http')) urls.push(link);
+					else urls.push(config.urls.nyt + link);
+				}
+			});
+		});
+
+		return urls;
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function get_cbs_urls() {
+	try {
+		const response = await axios.get(config.urls.cbs);
+
+		// If no response errors occurred continue
+		if (response.status !== 200)
+			throw new Error('Something went wrong when fetching home page');
+
+		const dom = new JSDOM(response.data);
+
+		let urls = [];
+
+		dom.window.document.querySelectorAll('.item__anchor').forEach((anchor) => {
+			const link = anchor.getAttribute('href');
+			if (!link.includes('video') && !link.includes('pictures'))
+				urls.push(link);
+		});
+
+		return urls;
 	} catch (error) {
 		throw error;
 	}
@@ -52,61 +75,108 @@ async function parse_nyt_article(url) {
 		const response = await axios.get(url);
 
 		// If no response errors occurred continue
-		if (response.status === 200) {
-			const $ = cheerio.load(response.data);
+		if (response.status !== 200)
+			throw new Error('Something went wrong when fetching home page');
 
-			// Extract headline, summary and body
-			const headline = $('h1').attr('itemprop', 'headline').text();
-			const summary = $('#article-summary').text();
-			const body = $('.StoryBodyCompanionColumn').find('p').text();
+		const dom = new JSDOM(response.data);
 
-			return { headline, summary, body };
-		} else {
-			throw new Error('Something went wrong when fetching article');
-		}
+		// Extract headline and body
+		const headline = dom.window.document.querySelector(
+			'h1[itemprop="headline"]'
+		).textContent;
+		let body = '';
+		dom.window.document
+			.querySelectorAll('.StoryBodyCompanionColumn')
+			.forEach((b) => {
+				b.querySelectorAll('p').forEach((p) => (body += p.textContent + ' '));
+			});
+
+		return { headline, body };
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function parse_cbs_article(url) {
+	try {
+		const response = await axios.get(url);
+
+		// If no response errors occurred continue
+		if (response.status !== 200)
+			throw new Error('Something went wrong when fetching home page');
+
+		const dom = new JSDOM(response.data);
+
+		// Extract headline and body
+		const headline = dom.window.document.querySelector('.content__title')
+			.textContent;
+
+		let body = '';
+		dom.window.document
+			.querySelectorAll('p')
+			.forEach((p) => (body += p.textContent));
+
+		return { headline, body };
 	} catch (error) {
 		throw error;
 	}
 }
 
 /**
- * Tag article body
- * @param {Object} body The article body
- */
-function tag_article(body) {
-	// Initialize tagger
-	const lexicon = new natural.Lexicon(
-		config.pos_tagger.language,
-		config.pos_tagger.default_category,
-		config.pos_tagger.default_category_capitalized
-	);
-	const ruleSet = new natural.RuleSet(config.pos_tagger.language);
-	const tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
-
-	// Initialize tokenizer
-	const tokenizer = new natural.WordTokenizer();
-
-	const tokenized_body = tokenizer.tokenize(body);
-	const result = tagger.tag(tokenized_body);
-	return result.taggedWords;
-}
-
-/**
  * Scrape and parse all New York Times home page articles.
  */
-async function get_articles() {
+async function fetch_articles() {
 	try {
+		const [nyt_urls, cbs_urls] = await Promise.all([
+			get_nyt_urls(),
+			get_cbs_urls(),
+		]);
+
+		console.log(
+			`Fetching ${nyt_urls.length} NYT articles and ${cbs_urls.length} CBS articles`
+		);
+		const urls = [...nyt_urls, ...cbs_urls];
 		let articles = [];
-		const urls = await get_nyt_urls();
-		for (let index = 0; index < urls.length; index++) {
-			const url = urls[index];
-			if (index === 1) break;
 
-			let article = await parse_nyt_article(url);
-			article.tags = tag_article(article.body);
+		for (let i = 0; i < urls.length; i++) {
+			const url = urls[i];
+			let parsed = {};
 
-			articles.push(article);
+			// If article already exists in database, don't parse it
+			if (await Article.exists({ url })) continue;
+
+			console.log(`Parsing ${url}`);
+
+			// If parsing error occurs, continue with the rest of the articles
+			try {
+				// Check if url is from nyt or cbs and parse accordingly
+				if (url.includes(config.urls.nyt))
+					parsed = await parse_nyt_article(url);
+				else parsed = await parse_cbs_article(url);
+			} catch (error) {
+				continue;
+			}
+
+			const pos_tags = nlpService.tag_text(parsed.body);
+
+			const article = new Article({
+				_id: mongoose.Types.ObjectId(),
+				url,
+				headline: parsed.headline,
+				body: parsed.body,
+				pos_tags,
+			});
+
+			const saved = await article.save();
+
+			articles.push({
+				id: saved._id,
+				url: saved.url,
+				headline: saved.headline,
+			});
 		}
+
+		console.log(`${articles.length} articles were successfully inserted`);
 
 		return articles;
 	} catch (error) {
@@ -116,7 +186,8 @@ async function get_articles() {
 
 module.exports = {
 	get_nyt_urls,
+	get_cbs_urls,
 	parse_nyt_article,
-	tag_article,
-	get_articles,
+	parse_cbs_article,
+	fetch_articles,
 };
--- src/article/article.model.js
+++ src/article/article.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-// Create the article model for mongoose
+// Create the article model for the database
 const articleSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	url: String,
 	headline: String,
 	body: String,
-	postags: [],
+	pos_tags: [],
 });
 
 module.exports = mongoose.model('Article', articleSchema);
